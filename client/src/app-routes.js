524c968b0f84a5938e9f2049131b0faa7d7f731c
shah3 <hr653326@dal.ca>
2022-02-26 12:42:35 -0400

    Remove unused vars Ref. issue#8

--- client/src/app-routes.js
+++ client/src/app-routes.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Authentication from "./components/authenication/Authentication"
 import LandingPage from "./components/landing/LandingPage";
 
 const AppRoutes = (props) => {
