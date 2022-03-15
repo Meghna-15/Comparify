e7ed15e35bf48e389df7b6e0c716699336b3a586
shah3 <hr653326@dal.ca>
2022-03-15 01:30:06 -0300

    Fix: Sonar issue

--- server/src/main/java/ca/dal/comparify/user/model/iam/authentication/UserPrincipal.java
+++ server/src/main/java/ca/dal/comparify/user/model/iam/authentication/UserPrincipal.java
@@ -9,100 +9,130 @@ import java.util.Objects;
 
 public class UserPrincipal implements UserDetails {
 
-  private String id;
+    private String id;
 
-  private String userIdentifier;
+    private String userIdentifier;
 
-  private String secret;
+    private String secret;
 
-  private Collection<GrantedAuthority> authorities;
+    private Collection<GrantedAuthority> authorities;
 
-  private boolean isLocked;
+    private boolean isLocked;
 
-  private boolean isActive;
+    private boolean isActive;
 
-  private boolean isAccountExpired;
+    private boolean isAccountExpired;
 
-  private boolean isSecretExpired;
+    private boolean isSecretExpired;
 
 
-  public UserPrincipal(String id, String userIdentifier, String secret,
-                       Collection<GrantedAuthority> authorities,
-                       boolean isLocked, boolean isActive, boolean isAccountExpired,
-                       boolean isSecretExpired) {
-    this.id = id;
-    this.userIdentifier = userIdentifier;
-    this.secret = secret;
-    this.authorities = authorities;
-    this.isLocked = isLocked;
-    this.isActive = isActive;
-    this.isAccountExpired = isAccountExpired;
-    this.isSecretExpired = isSecretExpired;
-  }
+    /**
+     * @param id
+     * @param userIdentifier
+     * @param secret
+     * @param authorities
+     *
+     * @author Harsh Shah
+     */
+    public UserPrincipal(String id, String userIdentifier, String secret,
+                         Collection<GrantedAuthority> authorities) {
+        this.id = id;
+        this.userIdentifier = userIdentifier;
+        this.secret = secret;
+        this.authorities = authorities;
+        this.isLocked = false;
+        this.isActive = true;
+        this.isAccountExpired = false;
+        this.isSecretExpired = false;
+    }
 
-  public static UserPrincipal create(UserIAMModel model) {
+    /**
+     * @param model
+     * @return
+     *
+     * @author Harsh Shah
+     */
+    public static UserPrincipal create(UserIAMModel model) {
 
-    UserAuthenticationModel authentication = model.getAuthentication();
+        UserAuthenticationModel authentication = model.getAuthentication();
 
-    return new UserPrincipal(model.getId(),
-            model.getUserIdentifier(),
-            authentication.getSecret(),
-            authentication.getAuthorities(),
-            authentication.isLocked(),
-            authentication.isActive(),
-            authentication.isAccountExpired(),
-            authentication.isSecretExpired());
-  }
+        return new UserPrincipal(model.getId(), model.getUserIdentifier(),
+                authentication.getSecret(), authentication.getAuthorities())
+                    .setLocked(authentication.isLocked())
+                    .setActive(authentication.isActive())
+                    .setAccountExpired(authentication.isAccountExpired())
+                    .setSecretExpired(authentication.isSecretExpired());
+    }
 
-  public String getId() {
-    return id;
-  }
+    public String getId() {
+        return id;
+    }
 
-  @Override
-  public String getUsername() {
-    return userIdentifier;
-  }
+    @Override
+    public String getUsername() {
+        return userIdentifier;
+    }
 
-  @Override
-  public String getPassword() {
-    return secret;
-  }
+    @Override
+    public String getPassword() {
+        return secret;
+    }
 
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return authorities;
-  }
+    @Override
+    public Collection<GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
 
-  @Override
-  public boolean isAccountNonExpired() {
-    return !isAccountExpired;
-  }
+    @Override
+    public boolean isAccountNonExpired() {
+        return !isAccountExpired;
+    }
 
-  @Override
-  public boolean isAccountNonLocked() {
-    return !isLocked;
-  }
+    @Override
+    public boolean isAccountNonLocked() {
+        return !isLocked;
+    }
 
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return !isSecretExpired;
-  }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return !isSecretExpired;
+    }
 
-  @Override
-  public boolean isEnabled() {
-    return isActive;
-  }
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    UserPrincipal that = (UserPrincipal) o;
-    return Objects.equals(id, that.id);
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserPrincipal that = (UserPrincipal) o;
+        return Objects.equals(id, that.id);
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(id);
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    private UserPrincipal setLocked(boolean locked) {
+        isLocked = locked;
+        return this;
+    }
+
+    private UserPrincipal setActive(boolean active) {
+        isActive = active;
+        return this;
+    }
+
+    private UserPrincipal setAccountExpired(boolean accountExpired) {
+        isAccountExpired = accountExpired;
+        return this;
+    }
+
+    private UserPrincipal setSecretExpired(boolean secretExpired) {
+        isSecretExpired = secretExpired;
+        return this;
+    }
 }
