1fed24fc40a47fd2f73a6d7a1df890a839992f65
meghna <mg841071@dal.ca>
2022-03-16 15:17:31 -0300

    signup-frontend

--- client/src/app-routes.js
+++ client/src/app-routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/landing/LandingPage";
+import Register from "./components/register";
 
 const AppRoutes = (props) => {
 
@@ -8,9 +9,11 @@ const AppRoutes = (props) => {
     <Router basename={process.env.REACT_APP_BASE_HREF}>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-      </Routes>
+        <Route path="/register" element={<Register />} />
+        </Routes>
     </Router>
   );
+  
 };
 
 export default AppRoutes;
--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,19 +1,17 @@
 import { Box } from "@material-ui/core";
 import React from "react";
-import Authentication from "../authenication/Authentication";
-import useStyles from "../../hooks/use-styles";
-
-
-import Link from '@mui/material/Link';
+import useStyles from "../hooks/use-styles";
+import RegisterPage from "./RegisterPage";
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
 import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
 
 
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
-import logo from './../../assets/images/logo.png';
+import logo from './../assets/images/logo.png';
 
 const style = {
     root: {
@@ -32,7 +30,7 @@ const style = {
     }
 }
 
-const LandingPage = (props) => {
+const Register = (props) => {
 
     const classes = useStyles(style);
 
@@ -48,27 +46,21 @@ const LandingPage = (props) => {
                         <LockOutlinedIcon />
                     </Avatar>
                     <Typography component="h1" variant="h5">
-                        Sign in
+                        Register
                     </Typography>
 
-                    <Authentication></Authentication>
-
-                    <Grid container>
-                        <Grid item xs>
-                            <Link href="#" variant="body2">
-                            Forgot password?
+                    <RegisterPage></RegisterPage>
+                    <Grid item>
+                            <Link href="/" variant="body2">
+                            {"Already have an account? Log In"}
                             </Link>
-                        </Grid>
-                        <Grid item>
-                            <Link href="/Register" variant="body2">
-                            {"Don't have an account? Sign Up"}
-                            </Link>
-                        </Grid>
                     </Grid>
+
+                    
                 </Box>
             </Grid>
         </Grid>
     </>)
 }
 
-export default LandingPage;
+export default Register;
