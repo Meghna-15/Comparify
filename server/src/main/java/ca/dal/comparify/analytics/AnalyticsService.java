6a9a71830ff2a7d289aa7692611933640f18035a
shah3 <hr653326@dal.ca>
2022-04-04 20:20:50 -0300

    Integrated Analytics Ref. issue #47

--- server/src/main/java/ca/dal/comparify/analytics/AnalyticsService.java
+++ server/src/main/java/ca/dal/comparify/analytics/AnalyticsService.java
@@ -1,10 +1,13 @@
 package ca.dal.comparify.analytics;
 
 import ca.dal.comparify.brand.model.BrandModel;
+import ca.dal.comparify.itemcategories.ItemCategoryService;
 import ca.dal.comparify.model.HashModel;
+import ca.dal.comparify.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -17,6 +20,9 @@ public class AnalyticsService {
 
     @Autowired
     private AnalyticsRepository analyticsRepository;
+
+    @Autowired
+    private ItemCategoryService itemCategoryService;
 
     /**
      * @param itemId
@@ -41,11 +47,41 @@ public class AnalyticsService {
         List<String> brand = brands.stream().map(itr -> (String) itr.get("_id")).collect(Collectors.toList());
 
         List<BrandModel> brandInfo = analyticsRepository.getBrands(brand);
-
         result.put("brands", brandInfo);
-
         return result;
-
     }
+
+
+    /**
+     * @param date
+     * @return
+     */
+    public List<HashModel> getProductCountForCategory(String date){
+        return analyticsRepository.getProductCountForCategory(DateUtils.parse(date));
+    }
+
+
+    public Map<String, Double> getMonthlyTotalPurchaseOfItemCategory(int month){
+
+        List<HashModel> resultSet = analyticsRepository.getMonthlyTotalPurchaseOfItemCategory(month);
+
+        Map<String, Double> stats = new HashMap<>();
+        for(Map<String, Object> resut: resultSet){
+            stats.put((String) resut.get("_id"), (Double) resut.get("totalPurchase"));
+        }
+
+        Map<String, String> categories = analyticsRepository.getAllCategories().stream()
+            .collect(Collectors.toMap(itr -> itr.getId().toString(), itr -> itr.getCategoryName()));
+
+        Map<String, Double> output = new HashMap<>();
+
+        for(Map.Entry<String, String> entry: categories.entrySet()){
+            output.put(entry.getValue(), stats.getOrDefault(entry.getKey(), 0.0));
+        }
+
+        return output;
+    }
+
+
 
 }
